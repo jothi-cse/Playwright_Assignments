1bd77892183793ee021e038c805ebf04c8bbab87
Jothipriyak <jothipriyak96@gmail.com>
2026-02-05 16:46:29 +0530

    Add files via upload

--- Week1.day1/Datatypes.js
+++ Week1.day1/Datatypes.js
@@ -1,1 +1,10 @@
-.
+let userName ="Jothi"
+let organization = "Testleaf"
+let contactNumber =123456789
+let knowsAutomation = true
+let usesPlaywright
+console.log("Username type is:" +typeof(userName))
+console.log("Orginazation type is :"+typeof(organization))
+console.log("Contact Number type is :"+typeof(contactNumber))
+console.log("Knows Automation type is :"+typeof(knowsAutomation))
+console.log("Uses Playwright type is:" +typeof(usesPlaywright));
